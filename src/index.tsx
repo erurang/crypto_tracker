30952827c0c7e6d4c7af15c7af16c1df4278f203
erurang <56789064+erurang@users.noreply.github.com>
2022-05-17 11:56:48 +0900

    routes + styled.d

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
+import Theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const Theme = {};
+
 
 root.render(
   <React.StrictMode>
